9ff863584bbb026b972c9607dbc6c3202bd1517d
Anthony Saliba <anthonysaliba93@gmail.com>
2024-05-25 19:09:39 +0300

    Add fade animations to transition between menus

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Transition } from "react-transition-group";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import AnimatedBackground from "./components/AnimatedBackground";
@@ -30,7 +31,26 @@ function App() {
   return (
     <div className="max-w-screen min-h-screen mx-auto flex flex-col justify-center items-center">
       <AnimatedBackground />
-      {loadMenu()}
+      <Transition
+        in={true}
+        timeout={500}
+        appear
+        key={currentMenu}
+        mountOnEnter
+        unmountOnExit
+      >
+        {(state) => (
+          <div
+            className={`transition-opacity duration-500 ${
+              state === "entering" || state === "entered"
+                ? "opacity-100"
+                : "opacity-0"
+            }`}
+          >
+            {loadMenu()}
+          </div>
+        )}
+      </Transition>
     </div>
   );
 }
